87290d6c9cf0a96ea7f9f4fee15d83943d5de785
RichiGM <garnicamejiaricardo@gmail.com>
2025-10-08 10:13:30 -0600

    prueba de conexion con koyeb

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const apiKeyHERE = "xJyVEAV83S41c79EjjrJU-cQOsiAOa5kU00s4anV6Ag";
-export const base_url = "systematic-reiko-optitrip-53bc6b42.koyeb.app/.netlify/functions/server";
+export const base_url = "systematic-reiko-optitrip-53bc6b42.koyeb.app/functions/server";

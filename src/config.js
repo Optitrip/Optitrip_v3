752d07aafc50e68c7af93d8e49f74febf156dd46
RichiGM <garnicamejiaricardo@gmail.com>
2026-02-16 12:07:21 -0600

    nueva url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const apiKeyHERE = "xJyVEAV83S41c79EjjrJU-cQOsiAOa5kU00s4anV6Ag";
-export const base_url = "https://boring-meredithe-optitrip2-05afb1ae.koyeb.app";
+export const base_url = "https://circles-news-seminar-beer.trycloudflare.com";
